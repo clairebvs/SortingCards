a7ba891cd5f559af1fa1d9221098558ccfa88fdc
ClaireBvs <clairembeauvais@gmail.com>
2018-05-08 16:14:12 -0600

    test to have a different current card

--- test/round_test.rb
+++ test/round_test.rb
@@ -4,6 +4,7 @@ require './lib/guess'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require 'pry'
 
 class RoundTest < Minitest::Test
 
@@ -57,9 +58,44 @@ class RoundTest < Minitest::Test
     round.record_guess("3 of Hearts")
     assert_equal 1, round.guesses.count
     refute round.guesses.empty?
-    # .empty?
     #  index
-    #  count
   end
 
+  def test_first_guesses_has_feedback
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.record_guess("3 of Hearts")
+    assert_equal 1, round.guesses.count
+    assert_equal "Correct!", round.guesses.first.feedback
+    # binding.pry
+  end
+
+  def test_it_has_a_different_current_card
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    current_card = card_2
+
+    assert_instance_of Card, round.current_card
+  end
+
+
+
+# round.record_guess("Jack of Diamonds")
+# => #<Guess:0x007ffdf19c8a00 @card=#<Card:0x007ffdf1820a90 @value="4", @suit="Clubs">, @response="Jack of Diamonds">
+# round.guesses.count
+# => 2
+# round.guesses.last.feedback
+# => "Incorrect."
+# round.number_correct
+# => 1
+# round.percent_correct
+# => 50
+# adding number variable to card class
 end
